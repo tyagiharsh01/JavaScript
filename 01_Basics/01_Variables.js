62339776870f6b8a054859d7ecf8a1a2bf1c1280
HARSH TYAGI <120437995+tyagiharsh01@users.noreply.github.com>
2023-10-26 12:12:21 +0530

    Done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,23 @@
+const accountId = 12345;
+let accountEmail = "Harshtyagi9898@gmail.com";
+var accountPassword = "1234567890";
+
+/*
+Prefer not to use var
+beacuse of block space and functional space
+*/
+accountCity = "Noida"
+
+//accountId = 453 Not Allowed In JS
+
+accountEmail="ht@ht.com"
+accountPassword= "ht@123"
+accountCity= "Saharanpur"
+let accountState;
+
+
+//console.log(accountEmail);
+//console.log(accountPassword)
+//console.log(accountCity);
+//console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
