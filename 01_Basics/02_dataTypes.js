9fc439945164cf9a2927c5fd01b9db2d1fbf5d78
HARSH TYAGI <120437995+tyagiharsh01@users.noreply.github.com>
2023-10-26 12:40:26 +0530

    DataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict" //treat all code as a newer version
+// code readiblity should be high
+
+// number => 2 to the power 53
+// boolean => true or false
+// bigint
+// string=>""
+// null =>standalone value
+// undefined
+// symbol =>  for identify unique component (Speciallly in React)
+
+//object
+let age = 22
+let name = "Harsh"
+
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
