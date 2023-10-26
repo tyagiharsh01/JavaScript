3e460771b7b7550bdf09b01f9633d22b548ceb6e
HARSH TYAGI <120437995+tyagiharsh01@users.noreply.github.com>
2023-10-26 16:47:44 +0530

    Conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "harsh";
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+//"33"=> 33
+//"33abc" => NaN
+//true =>1
+let isLoggedIn = "harsh";
+//1=>true
+//""=>false
+//"Harsh"=>true
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let a = 33;
+let stringNumber = String(a);
+console.log(stringNumber);
+console.log(typeof stringNumber);
