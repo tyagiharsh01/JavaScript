c8eb851d5fdc17a2c88da3ec13117cdaaabe50c4
HARSH TYAGI <120437995+tyagiharsh01@users.noreply.github.com>
2023-10-26 17:51:49 +0530

    uncomment all the console log

--- 01_Basics/04_comparsion.js
+++ 01_Basics/04_comparsion.js
@@ -1,17 +1,17 @@
 // maximum comparsion opreators give result in boolean
-//console.log(2==2)
-//console.log(2>1)
-//console.log(2<1)
-//console.log(2!=1)
+console.log(2==2)
+console.log(2>1)
+console.log(2<1)
+console.log(2!=1)
 
-//console.log("2">1);
-//console.log("02">1);
+console.log("2">1);
+console.log("02">1);
 
 // avoid these type of conversion
-//console.log(null>0);
-//console.log(null>=0);
-//console.log(null<=0)
-//console.log(null==0);
+console.log(null>0);
+console.log(null>=0);
+console.log(null<=0)
+console.log(null==0);
 
 
 // strict check ====
