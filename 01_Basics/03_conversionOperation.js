aed05d22698f5531bf42bdefe77bd78cb7153150
HARSH TYAGI <120437995+tyagiharsh01@users.noreply.github.com>
2023-10-26 17:14:43 +0530

    opreations Done

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,6 +1,6 @@
 let score = "harsh";
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 let valueInNumber = Number(score);
 //console.log(typeof valueInNumber);
 //console.log(valueInNumber)
@@ -12,9 +12,47 @@ let isLoggedIn = "harsh";
 //""=>false
 //"Harsh"=>true
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 let a = 33;
 let stringNumber = String(a);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+//------------------------Opreation------------------------
+
+let value = 33;
+let negValue = -value;
+//console.log(negValue);
+//console.log(2+2);
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**3)
+//console.log(2/2)
+//console.log(2%2);
+let str1 = "Hello"
+let str2 = " Harsh";
+let str3 = str1+str2;
+//console.log(str3);
+//console.log(1+"2")
+//console.log("2"+1)
+//console.log(1+"2"+1+2)
+//console.log(1+2+1+"2")
+//console.log(3+4%5/3)
+
+//console.log(+1+true)
+//console.log(+true-true)
+//console.log(+"")
+let num1 ,num2,num3
+num1=num2=num3 =2+2;
+console.log(num1)
+let counter = 1;
+counter++;
+console.log(counter);
+++counter;
+console.log(counter);
+
+
+
