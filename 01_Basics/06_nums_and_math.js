ac48030fee0ed9b0f4fc2ac60b47f61b08168760
HARSH TYAGI <120437995+tyagiharsh01@users.noreply.github.com>
2023-10-30 15:15:26 +0530

    math and num

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,29 @@
+const score =400;
+
+const balance  = new Number(100);
+//console.log(balance);
+//
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const otherNumber =123.8966;
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++Maths**************************
+//console.log(Math);
+//console.log(Math.abs(-4));
+//console.log(Math.round(3.4));
+//console.log(Math.ceil(4.6));
+//console.log(Math.floor(4.9));
+//console.log(Math.min(4,5,6,4,2,6));
+console.log(Math.random());
+console.log(Math.floor((Math.random()*10)+1));
+
+const min =1;
+const max = 6;
+
+//Math.floor(Math.random()*(max-min+1))+min
+console.log(Math.floor(Math.random()*(max-min+1))+min);
