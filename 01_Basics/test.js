3dda3ef610b7e8390fab9173a44015df016cc897
HARSH TYAGI <120437995+tyagiharsh01@users.noreply.github.com>
2023-10-30 12:03:41 +0530

    done

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,9 @@
 console.log("Harsh");
+const obj1 = { a: 1, b: 2,c:4 };
+const obj2 = { b: 3, c: 4,a:3,d:4 };
+const mergedObject = { ...obj1, ...obj2 };
+console.log(mergedObject)
+const array1 = [1, 2];
+const array2 = [3, 4];
+const concatenatedArray = [...array1, ...array2];
+console.log(concatenatedArray)
