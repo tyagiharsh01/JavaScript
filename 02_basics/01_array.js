0fe3dc4d7c22d8e7d45dd6d3bf4b08002a6d4b04
HARSH TYAGI <120437995+tyagiharsh01@users.noreply.github.com>
2023-10-30 16:49:49 +0530

    array methods

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,1 +1,36 @@
 //array
+const arr = [1,2,3,4,5,6];
+const myHeros = ['Shaktiman','Ironman','batman']
+const arr2 = new Array(1,3,4,6,2)
+//console.log(arr);
+
+
+//ArrayMethods--
+//arr.push(6);
+//arr.push(10);
+//arr.pop();
+//arr.unshift(9)
+//arr.shift()
+console.log(arr);
+//console.log(arr.includes(9));
+//console.log(arr.indexOf(3));
+const myarr = arr.join();
+console.log(myarr);
+console.log(typeof myarr);
+
+const myArr = [0, 1, 2, 3, 4, 5]
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
+
+
