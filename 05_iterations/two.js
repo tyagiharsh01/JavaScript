25f54462611e8bdda9dba57734509713671b600c
HARSH TYAGI <120437995+tyagiharsh01@users.noreply.github.com>
2023-11-03 17:23:56 +0530

    Basic Loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,16 @@
+index =0;
+while(index<10){
+    console.log("Hi");
+   index= index+2;
+}
+let myArr =["flash","batman","superman"];
+let arr =0;
+while(arr<myArr.length){
+    console.log(`value is ${myArr[arr]}`)
+    arr++;
+}
+let score =11;
+do{
+   console.log(`Score is ${score}`) ;
+   score++;
+}while (score<10)
